049238d7e486a6bb85165d50627c3e088553e225
Jeong Dong <eejxdong99@gmail.com>
2024-04-11 15:43:49 +0900

    Initial commit

---
+++ NC1-TIL/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NC1-TIL
+//
+//  Created by 이정동 on 4/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NC1-TIL/NC1_TILApp.swift
@@ -0,0 +1,17 @@
+//
+//  NC1_TILApp.swift
+//  NC1-TIL
+//
+//  Created by 이정동 on 4/11/24.
+//
+
+import SwiftUI
+
+@main
+struct NC1_TILApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
